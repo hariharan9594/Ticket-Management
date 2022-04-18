db0523b6702f1fbee440dfb1c6f1895bf9ec1735
Vipindas <vipin@cloudstick.io>
2022-04-18 05:16:32 +0000

    add new request initial user

--- routers/user.go
+++ routers/user.go
@@ -10,5 +10,6 @@ func SetUserRoutes(e *echo.Echo) *echo.Echo {
 	e.POST(versionpref+"/users", controllers.CreateUser, middleware.AdminRBAC)
 	e.POST(versionpref+"/login", controllers.Login)
 	e.POST(versionpref+"/test"+ "/users/:uid", controllers.SampleUserRequest, middleware.UserRBAC)
+	e.POST(versionpref+"/initial-user", controllers.CreateUser)
 	return e
 }
